e193593091ccb03d6f70da2ae1b804659a2cc409
Chan Lee <clee09818@gmail.com>
2022-07-24 14:15:37 -0400

    added JavaScript functions for button

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 // h2.textContent = "This content added by JavaScript";
 // document.querySelector("body").appendChild(h2);
 
+const foodOptions = ['Pizza', 'Hamburger', 'Hot Dog', 'Pasta', 'Salad','Fried Chicken'];
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function(){
+  let randomFood = Math.trunc(Math.random() * foodOptions.length)
+  document.getElementById('food').textContent = foodOptions[randomFood];
+})
